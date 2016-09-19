e4e23adf0068e53db490beac836f7fb31a932a40
ferry <ferryxo@yahoo.com>
2016-09-19 18:00:49 -0400

    changed the dictionary format

--- ReviewerAssignment.py
+++ ReviewerAssignment.py
@@ -5,7 +5,21 @@ import json
 
 app = Flask(__name__)
 
-class Submission:
+# base class that will make all derivatives JSON serializable:
+class JSONSerializable(list): # need to derive from a serializable class.
+
+  def __init__(self, value = None):
+    self = [ value ]
+
+  def setJSONSerializableValue(self, value):
+    self = [ value ]
+
+  def getJSONSerializableValue(self):
+    return self[1] if len(self) else None
+
+
+
+class Submission(JSONSerializable):
     def __init__(self, submission_id, team_id):
         self.submission_id = submission_id
         self.team_id = team_id
@@ -17,7 +31,7 @@ class Submission:
         return json.dumps(self, default=lambda o: o.__dict__,
             sort_keys=True, indent=4)
 
-class Reviewer:
+class Reviwer(JSONSerializable):
     def __init__(self, user_id, username, reputation, preference):
         self.user_id = user_id
         self.username = username
@@ -44,14 +58,14 @@ def assign_review_random(submissions, reviewers, max_n_review):
             for j in range(i, i+max_n_review):
                 reviewer_index = j if j + max_n_review < n_reviewer else j % n_reviewer
 
-                review_team.append(reviewers[reviewer_index].to_JSON())
-                if reviewers[reviewer_index].to_JSON() in reviewers_task_map.keys():
-                    reviewers_task_map[reviewers[reviewer_index].to_JSON()].append(submissions[i].to_JSON())
+                review_team.append(reviewers[reviewer_index])
+                if reviewers[reviewer_index]['reviewer_id'] in reviewers_task_map.keys():
+                    reviewers_task_map[reviewers[reviewer_index]['reviewer_id']].append(submissions[i])
                 else:
-                    this_reviewer_task = [submissions[i].to_JSON()]
-                    reviewers_task_map[reviewers[reviewer_index].to_JSON()] = this_reviewer_task
+                    this_reviewer_task = [submissions[i]]
+                    reviewers_task_map[reviewers[reviewer_index]['reviewer_id']] = this_reviewer_task
 
-            submission_reviewers_map[submissions[i].to_JSON()] = review_team
+            submission_reviewers_map[submissions[i]['submission_id']] = review_team
     else:
         raise ValueError('number of reviews per submission must be smaller than number of reviewers')
 
@@ -61,24 +75,24 @@ def assign_review_random(submissions, reviewers, max_n_review):
 @app.route('/')
 def hello_world():
 
-    submissions = [Submission('S00', 'T00'),
-                   Submission('S01', 'T01'),
-                   Submission('S02', 'T02'),
-                   Submission('S03', 'T03'),
-                   Submission('S04', 'T04'),]
+    submissions = [{'submission_id':'S00', 'team_id':'T00'},
+                   {'submission_id':'S01', 'team_id':'T01'},
+                   {'submission_id':'S02', 'team_id':'T02'},
+                   {'submission_id':'S03', 'team_id':'T03'},
+                   {'submission_id':'S04', 'team_id':'T04'}]
 
-    reviewers = [Reviewer('R00', 'Donald Trump', '0.5', 'S00'),
-                 Reviewer('R01', 'Hilary Clinton', '0.75', 'S00'),
-                 Reviewer('R02', 'Bart Simpson', '0.5', 'S01'),
-                 Reviewer('R03', 'Mickey Mouse', '0.4', 'S01'),
-                 Reviewer('R04', 'Minie Mouse', '0.8', 'S02'),
-                 Reviewer('R05', 'Oliver Quenn', '0.3', 'S02'),
-                 Reviewer('R06', 'Clark Kent', '0.5', 'S03'),
-                 Reviewer('R07', 'Bruce Wayne', '0.7', 'S03'),
-                 Reviewer('R08', 'Louise Lane', '0.5', 'S04'),
-                 Reviewer('R09', 'Lana Lang', '0.9', 'S04')]
+    reviewers = [{'reviewer_id':'R00', 'name':'Donald Trump', 'reputation':'0.5', 'preference':['S00']},
+                 {'reviewer_id':'R01', 'name':'Hilary Clinton', 'reputation':'0.75', 'preference':['S00']},
+                 {'reviewer_id':'R02', 'name':'Bart Simpson', 'reputation':'0.5', 'preference':['S01']},
+                 {'reviewer_id':'R03', 'name':'Mickey Mouse', 'reputation':'0.4', 'preference':['S01']},
+                 {'reviewer_id':'R04', 'name':'Minie Mouse', 'reputation':'0.8', 'preference':['S02']},
+                 {'reviewer_id':'R05', 'name':'Oliver Quenn', 'reputation':'0.3', 'preference':['S02']},
+                 {'reviewer_id':'R06', 'name':'Clark Kent', 'reputation':'0.5', 'preference':['S03']},
+                 {'reviewer_id':'R07', 'name':'Bruce Wayne', 'reputation':'0.7', 'preference':['S03']},
+                 {'reviewer_id':'R08', 'name':'Louise Lane', 'reputation':'0.5', 'preference':['S04']},
+                 {'reviewer_id':'R09', 'name':'Lana Lang', 'reputation':'0.9', 'preference':['S04']}]
 
-    assignment = assign_review_random(submissions, reviewers, 5)
+    assignment = assign_review_random(submissions, reviewers, 6)
 
     return assignment
 
